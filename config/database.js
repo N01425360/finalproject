30e3d59ea406d59d98097799d80321adeab81084
Manjot Kaur <90333611+N01425360@users.noreply.github.com>
2022-12-06 20:41:24 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
  
-    url : "mongodb+srv://admin:admin@clusterjeet.gthpi7h.mongodb.net/restaurants"
+    url : "mongodb+srv://admin:admin@clustermanjot.wbl3vcq.mongodb.net/restaurants"
 
 };
 
